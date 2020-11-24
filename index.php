41ff8bd4f629eb2a52166fdf9bc13fb445850d7e
alejandroch495 <72459744+alejandroch495@users.noreply.github.com>
2020-11-24 09:15:37 -0800

    added dump

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 //echo 'you are such a big fart looozer';
 if(isset($_GET["FirstName"])){//show feedback
 echo $_GET["FirstName"];
+echo '<pre>';
+var_dump($_GET);
+echo '</pre>';
+
 }
 else{//show form
     echo '<p>This is a form yay?</p><form action="">First Name: <input type="text" name="FirstName" placeholder="First Name" /><br />
